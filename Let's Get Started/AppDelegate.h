61ba9419afb7b5af51ba278d918c048e8a10bd61
Alan Barth <alancbarth@gmail.com>
2015-03-17 23:55:22 -0600

    Initial Commit

---
+++ Let's Get Started/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Let's Get Started
+//
+//  Created by Alan Barth on 3/17/15.
+//  Copyright (c) 2015 Alan Barth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Let's Get Started/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Let's Get Started
+//
+//  Created by Alan Barth on 3/17/15.
+//  Copyright (c) 2015 Alan Barth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
